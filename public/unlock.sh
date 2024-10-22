1f67c7e41effe4a5201f1f94acae74db3c03e7a1
Filippo Casarin <casarin.filippo17@gmail.com>
2024-10-22 16:40:59 +0200

    lock IPv6

--- public/unlock.sh
+++ public/unlock.sh
@@ -4,12 +4,12 @@ WS="131.114.2.138"
 DOMAIN="squadre.olinfo.it"  # usiamo vecchio dominio per retrocompatibilita'
                             # tanto sed cancella lo stesso la cosa giusta
 
-IPTABLES="/sbin/iptables"
-
-$IPTABLES -P INPUT ACCEPT
-$IPTABLES -P FORWARD ACCEPT
-$IPTABLES -P OUTPUT ACCEPT
-$IPTABLES -F
-$IPTABLES -X
+for IPTABLES in "/sbin/iptables" "/sbin/ip6tables"; do
+    $IPTABLES -P INPUT ACCEPT
+    $IPTABLES -P FORWARD ACCEPT
+    $IPTABLES -P OUTPUT ACCEPT
+    $IPTABLES -F
+    $IPTABLES -X
+done
 
 sed -i "/$DOMAIN/d" /etc/hosts
