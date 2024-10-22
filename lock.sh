aee3809b51cfd26566beacae9778b780ccc924cd
Virv12 <Virv12@users.noreply.github.com>
2024-10-22 14:41:32 +0000

    deploy: 1f67c7e41effe4a5201f1f94acae74db3c03e7a1

--- lock.sh
+++ lock.sh
@@ -3,18 +3,18 @@
 WS="131.114.2.138"
 DOMAIN="gara.squadre.olinfo.it"
 
-IPTABLES="/sbin/iptables"
+for IPTABLES in "/sbin/iptables" "/sbin/ip6tables"; do
+    $IPTABLES -P INPUT DROP
+    $IPTABLES -P OUTPUT DROP
+    $IPTABLES -P FORWARD DROP
 
-$IPTABLES -P INPUT DROP
-$IPTABLES -P OUTPUT DROP
-$IPTABLES -P FORWARD DROP
+    $IPTABLES -A INPUT -i lo -j ACCEPT
+    $IPTABLES -A OUTPUT -o lo -j ACCEPT
 
-$IPTABLES -A INPUT -i lo -j ACCEPT
-$IPTABLES -A OUTPUT -o lo -j ACCEPT
+    $IPTABLES -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+    $IPTABLES -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+done
 
-$IPTABLES -A OUTPUT -d $WS -p tcp -j ACCEPT
-
-$IPTABLES -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-$IPTABLES -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -A OUTPUT -d $WS -p tcp -j ACCEPT
 
 echo $WS $DOMAIN >> /etc/hosts
